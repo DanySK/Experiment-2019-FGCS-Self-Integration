0352f450b27862a51aa73b4e7070cff47eca7c48
Danilo Pianini <danilo.pianini@unibo.it>
2020-01-25 00:34:27 +0100

    Update autogit

--- autogit.sh
+++ autogit.sh
@@ -6,7 +6,7 @@ for file in data/*
   fi
 done
 COMMIT_MESSAGE="[AUTOMATIC ${USER}@${HOSTNAME}] - `date --iso-8601=minutes` data update"
-git commit -m $COMMIT_MESSAGE
+git commit -m "${COMMIT_MESSAGE}"
 git fetch
 git merge FETCH_HEAD -m "$COMMIT_MESSAGE (merge commit)"
 git push
